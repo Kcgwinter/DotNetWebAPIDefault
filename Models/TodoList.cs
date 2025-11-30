a60dac87d4001279025f5a05d5236651f3dc7358
Kevin Winter <kcgwinter@gmail.com>
2025-11-30 13:59:11 -0500

    rm pushed false files

--- Models/TodoList.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace DotNetWebAPIDefault.Models;
-
-public class TodoList : BaseModel
-{
-    public required string Name { get; set; }
-    public List<Todo>? Todos { get; set; }
-}
